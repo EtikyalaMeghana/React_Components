48ad38caaa57bd998adaa4cc8aaf9eb669771337
EtikyalaMeghana <41376617+EtikyalaMeghana@users.noreply.github.com>
2024-10-22 15:48:40 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Myh from "./Heading";
+import List from "./List";
+
+ReactDOM.render(
+  <div>
+    <Myh />
+    <List />
+  </div>,
+  document.getElementById("root")
+);
